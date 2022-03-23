a5d8df97bb49f381f70883052902c0699d6fd659
Antoine BOULARD <antoineboulard@gmx.fr>
2022-03-23 11:54:32 +0100

    Test Anto

--- src/main/java/com/example/projetlego/HelloApplication.java
+++ src/main/java/com/example/projetlego/HelloApplication.java
@@ -16,6 +16,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
         caca=1;
+        antoEstpassé=2;
     }
 
     public static void main(String[] args) {
