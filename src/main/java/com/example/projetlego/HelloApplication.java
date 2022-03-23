1a0fc0d452e4355adfa61e2f8819560060d82548
romeo <lariviereromeo@gmail.com>
2022-03-23 12:13:08 +0100

    Roméo

--- src/main/java/com/example/projetlego/HelloApplication.java
+++ src/main/java/com/example/projetlego/HelloApplication.java
@@ -15,7 +15,7 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-        caca=1;
+        romeoEstPassé=1;
         antoEstpassé=2;
         christianEstpassé=3;
     }
