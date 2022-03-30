0c4e865dad78cac13720c0f991d06a8a476c1cf8
romeo <lariviereromeo@gmail.com>
2022-03-30 11:39:20 +0200

    Interface recherche

--- src/main/java/com/example/projetlego/HelloApplication.java
+++ src/main/java/com/example/projetlego/HelloApplication.java
@@ -2,25 +2,22 @@ package com.example.projetlego;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-        romeoEstPassé=1;
-        antoEstpassé=2;
-        christianEstpassé=3;
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 
+
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
